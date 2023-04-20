af4cf3efcf8754805109c7dd44bc7c0f797c1ed9
genieCS <geniecs@github.com>
2023-04-21 06:02:00 +0900

    remove println

--- src/tetris.rs
+++ src/tetris.rs
@@ -55,7 +55,6 @@ impl View for Tetris {
     }
 
     fn on_event(&mut self, event: Event) -> EventResult {
-        println!("event occur");
         match event {
             Event::Refresh | Event::Key(Key::Down) => self.on_down(false),
             Event::Char(' ') => self.on_down(true),
