a5ce154fe3b7d01c05eb0f007eb4a5a9b9809454
genieCS <geniecs@github.com>
2023-04-21 20:06:15 +0900

    rename blocks according to tetris rule

--- src/shape.rs
+++ src/shape.rs
@@ -11,16 +11,18 @@ trait ShapeTr {
 
 #[derive(Debug, Clone, Copy)]
 pub enum Shape {
-    S1(S1),
-    S2(S2),
-    S3(S3),
-    S4(S4),
-    S5(S5),
+    SI(SI),
+    SO(SO),
+    ST(ST),
+    SS(SS),
+    SZ(SZ),
+    SJ(SJ),
+    SL(SL),
 }
 
 impl Default for Shape {
     fn default() -> Self {
-        let shapes: [Shape; 5] = [Shape::S1(S1::S1_1), Shape::S2(S2::S2_1), Shape::S3(S3::S3_1), Shape::S4(S4::S4_1), Shape::S5(S5::S5_1)];
+        let shapes: [Shape; 7] = [Shape::SI(SI::SI_1), Shape::SO(SO::SO_1), Shape::ST(ST::ST_1), Shape::SS(SS::SS_1),  Shape::SZ(SZ::SZ_1), Shape::SJ(SJ::SJ_1), Shape::SL(SL::SL_1)];
         shapes[rand::thread_rng().gen_range(0..shapes.len())]
     }
 }
@@ -28,184 +30,233 @@ impl Default for Shape {
 impl Shape {
     pub fn vectors(&self) -> Positions {
         match self {
-            Shape::S1(shape) => shape.vectors(),
-            Shape::S2(shape) => shape.vectors(),
-            Shape::S3(shape) => shape.vectors(),
-            Shape::S4(shape) => shape.vectors(),
-            Shape::S5(shape) => shape.vectors(),
+            Shape::SI(shape) => shape.vectors(),
+            Shape::SO(shape) => shape.vectors(),
+            Shape::ST(shape) => shape.vectors(),
+            Shape::SS(shape) => shape.vectors(),
+            Shape::SZ(shape) => shape.vectors(),
+            Shape::SJ(shape) => shape.vectors(),
+            Shape::SL(shape) => shape.vectors(),
         }
     }
 
     pub fn rotate(&self) -> Shape {
         match self {
-            Shape::S1(shape) => shape.rotate(),
-            Shape::S2(shape) => shape.rotate(),
-            Shape::S3(shape) => shape.rotate(),
-            Shape::S4(shape) => shape.rotate(),
-            Shape::S5(shape) => shape.rotate(),
+            Shape::SI(shape) => shape.rotate(),
+            Shape::SO(shape) => shape.rotate(),
+            Shape::ST(shape) => shape.rotate(),
+            Shape::SS(shape) => shape.rotate(),
+            Shape::SZ(shape) => shape.rotate(),
+            Shape::SJ(shape) => shape.rotate(),
+            Shape::SL(shape) => shape.rotate(),
         }
     }
 }
 
-//    S1_1      S1_2
+//    SI_1      SI_2
 //  _ _ _ _      _
 // |_|_|_|_| ,  |_|
 //              |_|
 //              |_|
 //              |_|
 #[derive(Debug, Clone, Copy)]
-pub enum S1 {
-    S1_1,
-    S1_2,
+pub enum SI {
+    SI_1,
+    SI_2,
 }
 
-impl ShapeTr for S1 {
+impl ShapeTr for SI {
     fn vectors(&self) -> Positions {
         match self {
-            S1::S1_1 => vec![Vec2::new(0,0),Vec2::new(1,0),Vec2::new(2,0),Vec2::new(3,0)],
-            S1::S1_2 => vec![Vec2::new(0,0),Vec2::new(0,1),Vec2::new(0,2),Vec2::new(0,3)],
+            SI::SI_1 => vec![Vec2::new(0,0),Vec2::new(1,0),Vec2::new(2,0),Vec2::new(3,0)],
+            SI::SI_2 => vec![Vec2::new(0,0),Vec2::new(0,1),Vec2::new(0,2),Vec2::new(0,3)],
         }
     }
 
     fn rotate(&self) -> Shape {
         match self {
-            S1::S1_1 => Shape::S1(S1::S1_2),
-            S1::S1_2 => Shape::S1(S1::S1_1),
+            SI::SI_1 => Shape::SI(SI::SI_2),
+            SI::SI_2 => Shape::SI(SI::SI_1),
         }
     }
 }
 
-//  S2_1     S2_2    S2_3     S2_4   
-//  _         _ _      _      _ _
-// |_|_     _|_|_|   _|_|    |_|_|_
-// |_|_|   |_|_|    |_|_|      |_|_|
-//   |_|,         , |_|    ,
-#[derive(Debug, Clone, Copy)]
-pub enum S2 {
-    S2_1,
-    S2_2,
-    S2_3,
-    S2_4,
-}
-
-impl ShapeTr for S2 {
-    fn vectors(&self) -> Positions {
-        match self {
-            S2::S2_1 => vec![Vec2::new(0,0),Vec2::new(0,1),Vec2::new(1,1),Vec2::new(1,2)],
-            S2::S2_2 => vec![Vec2::new(1,0),Vec2::new(2,0),Vec2::new(0,1),Vec2::new(1,1)],
-            S2::S2_3 => vec![Vec2::new(1,0),Vec2::new(0,1),Vec2::new(1,1),Vec2::new(0,2)],
-            S2::S2_4 => vec![Vec2::new(0,0),Vec2::new(1,1),Vec2::new(1,0),Vec2::new(2,1)],
-        }
-    }
-
-    fn rotate(&self) -> Shape {
-        match self {
-            S2::S2_1 => Shape::S2(S2::S2_2),
-            S2::S2_2 => Shape::S2(S2::S2_1),
-            S2::S2_3 => Shape::S2(S2::S2_4),
-            S2::S2_4 => Shape::S2(S2::S2_3),
-        }
-    }
-}
-
-//  S3_1
+//  SO_1
 //  _ _ 
 // |_|_|
 // |_|_|
 #[derive(Debug, Clone, Copy)]
-pub enum S3 {
-    S3_1,
+pub enum SO {
+    SO_1,
 }
 
-impl ShapeTr for S3 {
+impl ShapeTr for SO {
     fn vectors(&self) -> Positions {
         match self {
-            S3::S3_1 => vec![Vec2::new(0,0),Vec2::new(1,0),Vec2::new(0,1),Vec2::new(1,1)],
+            SO::SO_1 => vec![Vec2::new(0,0),Vec2::new(1,0),Vec2::new(0,1),Vec2::new(1,1)],
         }
     }
 
     fn rotate(&self) -> Shape {
         match self {
-            S3::S3_1 => Shape::S3(S3::S3_1),
+            SO::SO_1 => Shape::SO(SO::SO_1),
         }
     }
 }
 
-//    S4_1     S4_2    S4_3         S4_4    S4_5      S4_6    S4_7        S4_8
-//  _           _ _    _ _ _ _        _          _     _ _    _ _ _ _    _
-// |_|_ _ _    |_|_|  |_|_|_|_|      |_|   _ _ _|_|   |_|_|  |_|_|_|_|  |_|
-// |_|_|_|_|   |_|          |_|      |_|  |_|_|_|_|     |_|  |_|        |_|
-//             |_|                  _|_|                |_|             |_|_
-//           , |_|   ,          ,  |_|_|  ,         ,   |_| ,         , |_|_|
-#[derive(Debug, Clone, Copy)]
-pub enum S4 {
-    S4_1,
-    S4_2,
-    S4_3,
-    S4_4,
-    S4_5,
-    S4_6,
-    S4_7,
-    S4_8,
-}
 
-impl ShapeTr for S4 {
-    fn vectors(&self) -> Positions {
-        match self {
-            S4::S4_1 => vec![Vec2::new(0,0),Vec2::new(0,1),Vec2::new(1,1),Vec2::new(2,1), Vec2::new(3,1)],
-            S4::S4_2 => vec![Vec2::new(0,0),Vec2::new(1,0),Vec2::new(0,1),Vec2::new(0,2), Vec2::new(0,3)],
-            S4::S4_3 => vec![Vec2::new(0,0),Vec2::new(1,0),Vec2::new(2,0),Vec2::new(3,0), Vec2::new(3,1)],
-            S4::S4_4 => vec![Vec2::new(1,0),Vec2::new(1,1),Vec2::new(1,2),Vec2::new(1,3), Vec2::new(0,3)],
-            S4::S4_5 => vec![Vec2::new(3,0),Vec2::new(0,1),Vec2::new(1,1),Vec2::new(2,1), Vec2::new(3,1)],
-            S4::S4_6 => vec![Vec2::new(0,0),Vec2::new(1,0),Vec2::new(1,1),Vec2::new(1,2), Vec2::new(1,3)],
-            S4::S4_7 => vec![Vec2::new(0,0),Vec2::new(1,0),Vec2::new(2,0),Vec2::new(3,0), Vec2::new(0,1)],
-            S4::S4_8 => vec![Vec2::new(0,0),Vec2::new(0,1),Vec2::new(0,2),Vec2::new(0,3), Vec2::new(1,3)],
-        }
-    }
-
-    fn rotate(&self) -> Shape {
-        match self {
-            S4::S4_1 => Shape::S4(S4::S4_2),
-            S4::S4_2 => Shape::S4(S4::S4_3),
-            S4::S4_3 => Shape::S4(S4::S4_4),
-            S4::S4_4 => Shape::S4(S4::S4_1),
-            S4::S4_5 => Shape::S4(S4::S4_8),
-            S4::S4_6 => Shape::S4(S4::S4_5),
-            S4::S4_7 => Shape::S4(S4::S4_6),
-            S4::S4_8 => Shape::S4(S4::S4_7),
-        }
-    }
-}
-
-//  S5_1      S5_2    S5_3     S5_4   
+//  ST_1      ST_2     ST_3      ST_4   
 //    _       _                    _
 //  _|_|_    |_|_     _ _ _      _|_|
 // |_|_|_|   |_|_|   |_|_|_|    |_|_|
 //        ,  |_|   ,   |_|    ,   |_|
 #[derive(Debug, Clone, Copy)]
-pub enum S5 {
-    S5_1,
-    S5_2,
-    S5_3,
-    S5_4,
+pub enum ST {
+    ST_1,
+    ST_2,
+    ST_3,
+    ST_4,
 }
 
-impl ShapeTr for S5 {
+impl ShapeTr for ST {
     fn vectors(&self) -> Positions {
         match self {
-            S5::S5_1 => vec![Vec2::new(0,1),Vec2::new(1,0),Vec2::new(1,1),Vec2::new(2,1)],
-            S5::S5_2 => vec![Vec2::new(0,0),Vec2::new(0,1),Vec2::new(1,1),Vec2::new(0,2)],
-            S5::S5_3 => vec![Vec2::new(0,0),Vec2::new(1,0),Vec2::new(2,0),Vec2::new(1,1)],
-            S5::S5_4 => vec![Vec2::new(1,0),Vec2::new(0,1),Vec2::new(1,1),Vec2::new(1,2)],
+            ST::ST_1 => vec![Vec2::new(0,1),Vec2::new(1,0),Vec2::new(1,1),Vec2::new(2,1)],
+            ST::ST_2 => vec![Vec2::new(0,0),Vec2::new(0,1),Vec2::new(1,1),Vec2::new(0,2)],
+            ST::ST_3 => vec![Vec2::new(0,0),Vec2::new(1,0),Vec2::new(2,0),Vec2::new(1,1)],
+            ST::ST_4 => vec![Vec2::new(1,0),Vec2::new(0,1),Vec2::new(1,1),Vec2::new(1,2)],
         }
     }
 
     fn rotate(&self) -> Shape {
         match self {
-            S5::S5_1 => Shape::S5(S5::S5_2),
-            S5::S5_2 => Shape::S5(S5::S5_3),
-            S5::S5_3 => Shape::S5(S5::S5_4),
-            S5::S5_4 => Shape::S5(S5::S5_1),
+            ST::ST_1 => Shape::ST(ST::ST_2),
+            ST::ST_2 => Shape::ST(ST::ST_3),
+            ST::ST_3 => Shape::ST(ST::ST_4),
+            ST::ST_4 => Shape::ST(ST::ST_1),
+        }
+    }
+}
+
+//  SS_1     SS_2 
+//  _         _ _ 
+// |_|_     _|_|_|
+// |_|_|   |_|_|  
+//   |_|,         
+#[derive(Debug, Clone, Copy)]
+pub enum SS {
+    SS_1,
+    SS_2,
+}
+
+impl ShapeTr for SS {
+    fn vectors(&self) -> Positions {
+        match self {
+            SS::SS_1 => vec![Vec2::new(0,0),Vec2::new(0,1),Vec2::new(1,1),Vec2::new(1,2)],
+            SS::SS_2 => vec![Vec2::new(1,0),Vec2::new(2,0),Vec2::new(0,1),Vec2::new(1,1)],
+        }
+    }
+
+    fn rotate(&self) -> Shape {
+        match self {
+            SS::SS_1 => Shape::SS(SS::SS_2),
+            SS::SS_2 => Shape::SS(SS::SS_1),
+        }
+    }
+}
+
+//   SZ_1       SZ_2
+//   _ _         _
+//  |_|_|_     _|_|
+//    |_|_|   |_|_|
+//          , |_|
+#[derive(Debug, Clone, Copy)]
+pub enum SZ {
+    SZ_1,
+    SZ_2,
+}
+
+impl ShapeTr for SZ {
+    fn vectors(&self) -> Positions {
+        match self {
+            SZ::SZ_1 => vec![Vec2::new(0,0),Vec2::new(1,0),Vec2::new(1,1),Vec2::new(2,1)],
+            SZ::SZ_2 => vec![Vec2::new(1,0),Vec2::new(0,1),Vec2::new(1,1),Vec2::new(0,2)],
+        }
+    }
+
+    fn rotate(&self) -> Shape {
+        match self {
+            SZ::SZ_1 => Shape::SZ(SZ::SZ_2),
+            SZ::SZ_2 => Shape::SZ(SZ::SZ_1),
+        }
+    }
+}
+
+//    SJ_1     SJ_2    SJ_3         SJ_4   
+//  _           _ _    _ _ _ _        _    
+// |_|_ _ _    |_|_|  |_|_|_|_|      |_|    
+// |_|_|_|_|   |_|          |_|      |_|  
+//             |_|                  _|_|    
+//           , |_|   ,          ,  |_|_|    
+#[derive(Debug, Clone, Copy)]
+pub enum SJ {
+    SJ_1,
+    SJ_2,
+    SJ_3,
+    SJ_4,
+}
+
+impl ShapeTr for SJ {
+    fn vectors(&self) -> Positions {
+        match self {
+            SJ::SJ_1 => vec![Vec2::new(0,0),Vec2::new(0,1),Vec2::new(1,1),Vec2::new(2,1), Vec2::new(3,1)],
+            SJ::SJ_2 => vec![Vec2::new(0,0),Vec2::new(1,0),Vec2::new(0,1),Vec2::new(0,2), Vec2::new(0,3)],
+            SJ::SJ_3 => vec![Vec2::new(0,0),Vec2::new(1,0),Vec2::new(2,0),Vec2::new(3,0), Vec2::new(3,1)],
+            SJ::SJ_4 => vec![Vec2::new(1,0),Vec2::new(1,1),Vec2::new(1,2),Vec2::new(1,3), Vec2::new(0,3)],
+        }
+    }
+
+    fn rotate(&self) -> Shape {
+        match self {
+            SJ::SJ_1 => Shape::SJ(SJ::SJ_2),
+            SJ::SJ_2 => Shape::SJ(SJ::SJ_3),
+            SJ::SJ_3 => Shape::SJ(SJ::SJ_4),
+            SJ::SJ_4 => Shape::SJ(SJ::SJ_1),
+        }
+    }
+}
+
+
+//   SL_1      SL_2     SL_3    SL_4
+//      _      _ _    _ _ _      _ 
+//  _ _|_|    |_|_|  |_|_|_|    |_|
+// |_|_|_|      |_|  |_|        |_|_
+//          ,   |_| ,         , |_|_|
+ 
+#[derive(Debug, Clone, Copy)]
+pub enum SL {
+    SL_1,
+    SL_2,
+    SL_3,
+    SL_4,
+}
+
+impl ShapeTr for SL {
+    fn vectors(&self) -> Positions {
+        match self {
+            SL::SL_1 => vec![Vec2::new(0,1),Vec2::new(1,1),Vec2::new(2,1),Vec2::new(2,1), Vec2::new(3,1)],
+            SL::SL_2 => vec![Vec2::new(0,0),Vec2::new(1,0),Vec2::new(0,1),Vec2::new(0,2), Vec2::new(0,3)],
+            SL::SL_3 => vec![Vec2::new(0,0),Vec2::new(1,0),Vec2::new(2,0),Vec2::new(3,0), Vec2::new(3,1)],
+            SL::SL_4 => vec![Vec2::new(1,0),Vec2::new(1,1),Vec2::new(1,2),Vec2::new(1,3), Vec2::new(0,3)],
+        }
+    }
+
+    fn rotate(&self) -> Shape {
+        match self {
+            SL::SL_1 => Shape::SL(SL::SL_4),
+            SL::SL_2 => Shape::SL(SL::SL_1),
+            SL::SL_3 => Shape::SL(SL::SL_2),
+            SL::SL_4 => Shape::SL(SL::SL_3),
         }
     }
 }
