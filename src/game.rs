39584bdbf2190ee861b89255ecb164af408360fa
genieCS <geniecs@github.com>
2023-04-27 15:51:17 +0900

    refactor call back in game

--- src/game.rs
+++ src/game.rs
@@ -3,9 +3,8 @@ use crate::tetris::Tetris;
 use crate::manual::Manual;
 use cursive::{
     Cursive, CursiveExt,
-    event::{Event, Key},
-    view::Nameable,
-    views::{Dialog, LinearLayout, DummyView}, View,
+    view::{Nameable, Selector},
+    views::{Dialog, LinearLayout, DummyView},
 };
 
 pub fn run() {
@@ -22,58 +21,8 @@ pub fn run() {
                 .child(tetris)
     ).title("Tetris");
 
-    // let events = [
-    //     Event::Key(Key::Up),
-    //     Event::Key(Key::Down),
-    //     Event::Key(Key::Left),
-    //     Event::Key(Key::Right),
-    //     Event::Char(' '),
-    //     Event::Refresh,
-    // ];
-
-    siv.add_global_callback(Event::Refresh, |s| {
-        s.call_on_name("tetris", |tetris: &mut Tetris| {
-            tetris.on_event(Event::Refresh);
-        });
-    });
-    siv.add_global_callback(Event::Key(Key::Up), |s| {
-        s.call_on_name("tetris", |tetris: &mut Tetris| {
-            tetris.on_event(Event::Key(Key::Up));
-        });
-    });
-    siv.add_global_callback(Event::Key(Key::Down), |s| {
-        s.call_on_name("tetris", |tetris: &mut Tetris| {
-            tetris.on_event(Event::Key(Key::Down));
-        });
-    });
-    siv.add_global_callback(Event::Key(Key::Left), |s| {
-        s.call_on_name("tetris", |tetris: &mut Tetris| {
-            tetris.on_event(Event::Key(Key::Left));
-        });
-    });
-    siv.add_global_callback(Event::Key(Key::Right), |s| {
-        s.call_on_name("tetris", |tetris: &mut Tetris| {
-            tetris.on_event(Event::Key(Key::Right));
-        });
-    });
-    siv.add_global_callback(Event::Char(' '), |s| {
-        s.call_on_name("tetris", |tetris: &mut Tetris| {
-            tetris.on_event(Event::Char(' '));
-        });
-    });
-    siv.add_global_callback(Event::Char('n'), |s| {
-        s.call_on_name("tetris", |tetris: &mut Tetris| {
-            tetris.on_event(Event::Char('n'));
-        });
-    });
-    siv.add_global_callback(Event::Char('s'), |s| {
-        s.call_on_name("tetris", |tetris: &mut Tetris| {
-            tetris.on_event(Event::Char('s'));
-        });
-    });
-
-
     siv.add_layer(view);
+    siv.focus(&Selector::Name("tetris")).unwrap();
 
     siv.set_fps(1);
 
