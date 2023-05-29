0422da9e87529bad80b9af264fb6bb2e5dadbe4a
genieCS <geniecs@github.com>
2023-05-29 22:05:03 +0900

    change keymap

--- src/manual.rs
+++ src/manual.rs
@@ -22,13 +22,13 @@ impl View for Manual {
     fn draw(&self, printer: &Printer) {
         let y_padding = 6;
             printer.print((0, y_padding), &format!(" {:18} ", "Manual"));
-            printer.print((0 ,y_padding + 1), &format!(" {:18} ", "↑,d: rotate clockwise"));
-            printer.print((0 ,y_padding + 2), &format!(" {:18} ", "s: rotate counterclockwise"));
+            printer.print((0 ,y_padding + 1), &format!(" {:18} ", "↑,e: rotate clockwise"));
+            printer.print((0 ,y_padding + 2), &format!(" {:18} ", "w: rotate counterclockwise"));
             printer.print((0, y_padding + 3), &format!(" {:18} ", "↓: speed up"));
             printer.print((0, y_padding + 4), &format!(" {:18} ", "←: left"));
-            printer.print((0, y_padding + 5), &format!(" {:18} ", "z: left most"));
+            printer.print((0, y_padding + 5), &format!(" {:18} ", "a: left most"));
             printer.print((0, y_padding + 6), &format!(" {:18} ", "→: right"));
-            printer.print((0, y_padding + 7), &format!(" {:18} ", "x: right most"));
+            printer.print((0, y_padding + 7), &format!(" {:18} ", "d: right most"));
             printer.print((0, y_padding + 8), &format!(" {:18} ", "space: hard drop"));
             printer.print((0, y_padding + 9), &format!(" {:18} ", "m: stop and resume"));
             printer.print((0, y_padding + 10), &format!(" {:18} ", "n: new game"));
